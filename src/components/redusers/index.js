2a6d5a4a40fc2d59f98d497e683b850d1f946050
Ilya <ilyaphotoshop17@gmail.com>
2019-12-12 03:09:32 +0200

    [add] data preparation for product page

--- src/components/redusers/index.js
+++ src/components/redusers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from "redux";
 import {connectRouter} from "connected-react-router";
 
 import phones from "./phones";
+import phonesPage from "./phonesPage";
 
 export default history => combineReducers({
     phones,
+    phonesPage,
     router: connectRouter(history)
 })
---
+++ src/components/redusers/phonesPage.js
@@ -0,0 +1,18 @@
+import  * as R from 'ramda'
+
+import {FETCH_PHONES_SUCCESS} from "../actionType";
+
+const initialState = {
+    ids: []
+}
+
+export default (state = initialState, {type, payload}) => {
+    switch (type) {
+        case FETCH_PHONES_SUCCESS:
+           return  R.merge(state, {                  //получаем масив id-шников
+               ids: R.pluck('id', payload)
+           })
+        default:
+            return state
+    }
+}
