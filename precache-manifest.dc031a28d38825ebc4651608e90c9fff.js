80efe94aef5eab632e2c02db38ab88d2296a9c62
Ilya <ilyaphotoshop17@gmail.com>
2020-04-15 23:49:48 +0300

    Updates

---
+++ precache-manifest.dc031a28d38825ebc4651608e90c9fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e32ca6a8d5f9d2543ebe0a47537303",
+    "url": "/internetShop_React_Redux/index.html"
+  },
+  {
+    "revision": "f0a588836be83c8ef6a7",
+    "url": "/internetShop_React_Redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "61279dc38bf4334c45db",
+    "url": "/internetShop_React_Redux/static/js/2.1bb3268c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/internetShop_React_Redux/static/js/2.1bb3268c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0a588836be83c8ef6a7",
+    "url": "/internetShop_React_Redux/static/js/main.17236080.chunk.js"
+  },
+  {
+    "revision": "7ed0e89ab73a649fa9cf",
+    "url": "/internetShop_React_Redux/static/js/runtime-main.8872dfec.js"
+  }
+]);
